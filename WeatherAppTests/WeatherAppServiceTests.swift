3536a96e2284fa768254692742e3b7bef7f76d7a
areslui <brans0000@gmail.com>
2020-03-22 15:59:37 +0800

    chore(test): add test failed api call funciton

--- WeatherAppTests/WeatherAppServiceTests.swift
+++ WeatherAppTests/WeatherAppServiceTests.swift
@@ -24,6 +24,7 @@ class WeatherAppServiceTests: XCTestCase {
     super.tearDown()
   }
   
+  // Asynchronous test: success fast, failure slow
   func testSucessServiceCall() {
     let url = URL(string: "https://api.worldweatheronline.com/premium/v1/weather.ashx")
     
@@ -47,5 +48,32 @@ class WeatherAppServiceTests: XCTestCase {
     dataTask.resume()
     // then
     wait(for: [promise], timeout: 5)
+  }
+  
+  // Asynchronous test: faster fail
+  func testCallCompletes() {
+    // given
+    let url = URL(string: "https://api.worldweatheronline.com/premium/v1/weather.ashx")
+    // 1
+    let postParameters = ["key": "d0db1305b6964712bf630042202103", "q": "New+York"]
+    let request = RequestFactory.request(.POST, url!, postParameters)
+    let promise = expectation(description: "Completion handler invoked")
+    var statusCode: Int?
+    var responseError: Error?
+    
+    // when
+    let dataTask = sut.dataTask(with: request) { data, response, error in
+      statusCode = (response as? HTTPURLResponse)?.statusCode
+      responseError = error
+      // 2
+      promise.fulfill()
+    }
+    dataTask.resume()
+    // 3
+    wait(for: [promise], timeout: 5)
+    
+    // then
+    XCTAssertNil(responseError)
+    XCTAssertEqual(statusCode, 200)
   }
 }
