92f2a3b04fa252fcd37d4bef3468534b957d0d16
areslui <brans0000@gmail.com>
2020-03-29 11:48:03 +0800

    style(UI): set up UI for detail view

--- WeatherApp/View Controller/DetailViewController.swift
+++ WeatherApp/View Controller/DetailViewController.swift
@@ -12,8 +12,29 @@ import UIKit
 class DetailViewController: UIViewController {
   
   var weatherObj: Weather?
+  @IBOutlet weak var cityName: UILabel!
+  @IBOutlet weak var weather: UILabel!
+  @IBOutlet weak var tempC: UILabel!
+  @IBOutlet weak var weatherIcon: UIImageView!
+  @IBOutlet weak var currentTime: UILabel!
+  @IBOutlet weak var humidity: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupUI()
+  }
+  
+  private func setupUI() {
+    guard let weatherObj = weatherObj else { return }
+    cityName.text = weatherObj.city
+    weather.text = weatherObj.weather
+    if let tempC = weatherObj.temparature {
+      self.tempC.text = tempC + "\u{00B0}C"
+    }
+    if let imageData = weatherObj.imageData {
+      weatherIcon.image = UIImage.init(data: imageData)
+    }
+//    currentTime.text = weatherObj.date
+    humidity.text = weatherObj.humidity
   }
 }
